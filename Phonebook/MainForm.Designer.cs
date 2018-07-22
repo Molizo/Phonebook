d8a401003022b5c925c72d8ad95d0a51fa2be541
Visoiu Mihnea Theodor <visoiu.mihnea@gmail.com>
2018-07-22 18:03:49 +0300

    Added GUI

---
+++ Phonebook/MainForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Phonebook
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.ColumnFirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnMiddleName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnLastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnMobilePhone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnHomePhone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnFaxPhone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnWorkPhone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStripButtonAdd = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButtonRemove = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonAdd,
+            this.toolStripSeparator1,
+            this.toolStripButtonRemove});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(822, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnFirstName,
+            this.ColumnMiddleName,
+            this.ColumnLastName,
+            this.ColumnMobilePhone,
+            this.ColumnHomePhone,
+            this.ColumnFaxPhone,
+            this.ColumnWorkPhone,
+            this.ColumnEmail});
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.Location = new System.Drawing.Point(0, 25);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.Size = new System.Drawing.Size(822, 460);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // ColumnFirstName
+            // 
+            this.ColumnFirstName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.ColumnFirstName.HeaderText = "First Name";
+            this.ColumnFirstName.Name = "ColumnFirstName";
+            this.ColumnFirstName.ReadOnly = true;
+            this.ColumnFirstName.Width = 82;
+            // 
+            // ColumnMiddleName
+            // 
+            this.ColumnMiddleName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.ColumnMiddleName.HeaderText = "Middle Name";
+            this.ColumnMiddleName.Name = "ColumnMiddleName";
+            this.ColumnMiddleName.ReadOnly = true;
+            this.ColumnMiddleName.Width = 94;
+            // 
+            // ColumnLastName
+            // 
+            this.ColumnLastName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.ColumnLastName.HeaderText = "Last Name";
+            this.ColumnLastName.Name = "ColumnLastName";
+            this.ColumnLastName.ReadOnly = true;
+            this.ColumnLastName.Width = 83;
+            // 
+            // ColumnMobilePhone
+            // 
+            this.ColumnMobilePhone.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.ColumnMobilePhone.HeaderText = "Mobile Phone";
+            this.ColumnMobilePhone.Name = "ColumnMobilePhone";
+            this.ColumnMobilePhone.ReadOnly = true;
+            this.ColumnMobilePhone.Width = 97;
+            // 
+            // ColumnHomePhone
+            // 
+            this.ColumnHomePhone.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.ColumnHomePhone.HeaderText = "Home Phone";
+            this.ColumnHomePhone.Name = "ColumnHomePhone";
+            this.ColumnHomePhone.ReadOnly = true;
+            this.ColumnHomePhone.Width = 94;
+            // 
+            // ColumnFaxPhone
+            // 
+            this.ColumnFaxPhone.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.ColumnFaxPhone.HeaderText = "Fax Phone";
+            this.ColumnFaxPhone.Name = "ColumnFaxPhone";
+            this.ColumnFaxPhone.ReadOnly = true;
+            this.ColumnFaxPhone.Width = 83;
+            // 
+            // ColumnWorkPhone
+            // 
+            this.ColumnWorkPhone.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.ColumnWorkPhone.HeaderText = "Work Phone";
+            this.ColumnWorkPhone.Name = "ColumnWorkPhone";
+            this.ColumnWorkPhone.ReadOnly = true;
+            this.ColumnWorkPhone.Width = 92;
+            // 
+            // ColumnEmail
+            // 
+            this.ColumnEmail.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.ColumnEmail.HeaderText = "E-Mail";
+            this.ColumnEmail.Name = "ColumnEmail";
+            this.ColumnEmail.ReadOnly = true;
+            this.ColumnEmail.Width = 61;
+            // 
+            // toolStripButtonAdd
+            // 
+            this.toolStripButtonAdd.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripButtonAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonAdd.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonAdd.Image")));
+            this.toolStripButtonAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonAdd.Name = "toolStripButtonAdd";
+            this.toolStripButtonAdd.Size = new System.Drawing.Size(76, 22);
+            this.toolStripButtonAdd.Text = "Add contact";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButtonRemove
+            // 
+            this.toolStripButtonRemove.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonRemove.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonRemove.Image")));
+            this.toolStripButtonRemove.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonRemove.Name = "toolStripButtonRemove";
+            this.toolStripButtonRemove.Size = new System.Drawing.Size(97, 22);
+            this.toolStripButtonRemove.Text = "Remove contact";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 485);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.toolStrip1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Phonebook";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnFirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnMiddleName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnLastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnMobilePhone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnHomePhone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnFaxPhone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnWorkPhone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnEmail;
+        private System.Windows.Forms.ToolStripButton toolStripButtonAdd;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonRemove;
+    }
+}
+
